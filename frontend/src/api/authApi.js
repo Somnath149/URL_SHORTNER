12b9bc3331bb4ddf3c9b5eb503b74d1540347ba1
Somnath149 <patilsomnath149@gmail.com>
2026-01-20 07:27:33 +0530

    Update authApi.js

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/api/user';
+const API_URL = 'https://url-shortner-backend-ccsi.onrender.com/api/user';
 
 const axiosInstance = axios.create({
     baseURL: API_URL,
